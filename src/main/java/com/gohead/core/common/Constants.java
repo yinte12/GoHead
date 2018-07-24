b545ba37a7f9fe4d0203e5c9fe154141c0a87d5e
Super <yinte112@163.com>
2018-07-24 11:20:10 +0800

    feature: add token

--- src/main/java/com/gohead/core/common/Constants.java
+++ src/main/java/com/gohead/core/common/Constants.java
@@ -18,4 +18,15 @@ public class Constants {
 
     public static final String RESULT_EMPTY_REQUEST = "暂无数据";
 
+
+    /**
+     * jwt
+     */
+    public static final String JWT_ID = "jwt";
+    public static final String JWT_SECRET = "7786df7fc3a34e26a61c034d5ec8245d";
+    public static final int JWT_TTL = 60 * 60 * 1000;  //millisecond
+    public static final int JWT_REFRESH_INTERVAL = 55 * 60 * 1000;  //millisecond
+    public static final int JWT_REFRESH_TTL = 12 * 60 * 60 * 1000;  //millisecond
+
+
 }
