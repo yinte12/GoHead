98aedfc1111b005037754fb5fdc6944c331abef8
Super <yinte112@163.com>
2018-09-06 17:18:16 +0800

    feature：put 跨域

--- src/main/java/com/gohead/core/filters/SimpleCORSFilter.java
+++ src/main/java/com/gohead/core/filters/SimpleCORSFilter.java
@@ -19,7 +19,7 @@ public class SimpleCORSFilter implements Filter {
                          FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "*");
         chain.doFilter(req, response);
